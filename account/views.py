0e291b3a809d86bbf22585d4b5b4310f6661bca7
tnnmigga <tnnmigga@outlook.com>
2020-03-29 22:10:39 +0800

    支持更新用户资料

--- account/views.py
+++ account/views.py
@@ -69,7 +69,7 @@ def profile(request):
             user.username = request.POST['username']
             user.first_name = request.POST['first_name']
             user.last_name = request.POST['last_name']
-            #user.save()
+            user.save()
             template_args = {
                 'user': user,
                 'msg': '修改成功'
