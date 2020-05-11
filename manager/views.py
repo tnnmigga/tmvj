aaf81366f6fd2b8f5412de2aad0f95f973938858
tnnmigga <tnnmigga@outlook.com>
2020-05-12 00:04:00 +0800

    开始编写爬虫

--- manager/views.py
+++ manager/views.py
@@ -4,15 +4,19 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def manager_view(request):
+    return redirect('http://127.0.0.1:8000/admin/')
+    '''
     user_id = request.session.get('_auth_user_id')
     user = User.objects.get(pk=user_id)
     if user.is_staff:
         template_args = {
             'user': user,
         }
-        return render(request, 'manager_view.html', template_args)
+        return redirect('admin')
+        #return render(request, 'manager_view.html', template_args)
     else:
         return redirect('login')
+    '''
 
 def manage_user_profile_view(request):
     if request.method == 'GET':
